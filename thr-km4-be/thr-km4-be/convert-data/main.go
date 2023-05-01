8aa6e652035cb8810f90c2ca1eedc97bcab0f986
Indah Wahyu <indahramadhani2412@gmail.com>
2023-05-01 19:39:24 +0700

    upload tugas thr

---
+++ thr-km4-be/thr-km4-be/convert-data/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type User struct {
+	Name    string
+	Age     int
+	Address string
+}
+
+func ConvertData(data string) User {
+	fields := strings.Split(data, ",")
+	age, _ := strconv.Atoi(fields[1])
+	return User{Name: fields[0], Age: age, Address: fields[2]}
+}
+
+func main() {
+	data1 := "Edo,20,Jakarta"
+	user1 := ConvertData(data1)
+	fmt.Printf("User 1: %+v\n", user1)
+
+	data2 := "Budi,30,Bandung"
+	user2 := ConvertData(data2)
+	fmt.Printf("User 2: %+v\n", user2)
+}
+
+/*
+Format %+v pada fmt.Printf digunakan untuk mencetak nilai dari sebuah variabel
+dengan menampilkan nama propertinya pada struct.
+Karakter + pada format %+v digunakan untuk
+menampilkan label nama properti pada struct saat melakukan pencetakan nilai variabel.
+*/
